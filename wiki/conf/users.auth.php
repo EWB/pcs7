a73b4eebc3eccdfbe3bf29ff869d9573ed1481c0
EWB <ewilliamsbergen@stlawu.edu>
2016-07-13 09:40:15 -0500

    updating drupal core views and features

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$qJsuvLip$C7IRZrN.ZZ0aF7JxC9O3I.:Name:admin@stage.potsdamcoop.com:admin,user
+readonly:$1$koGTZgWn$ZSDRKKislGKcTjKbXEOMv/:readonly:admin@stage.potscamcoop.com:readonly
